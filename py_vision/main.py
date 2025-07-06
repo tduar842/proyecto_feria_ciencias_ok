7d7fd80548575a980fa04cddd8eb1e52f6d25446
tduar842 <159294094+tduar842@users.noreply.github.com>
2025-07-05 23:47:07 -0300

    Update main.py

--- py_vision/main.py
+++ py_vision/main.py
@@ -32,23 +32,7 @@ while True:
             result = face_recognizer.predict(rostro)
 
             cv2.putText(frame_input,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-            '''
-            # EigenFaces
-            if result[1] < 5700:
-                cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-            else:
-                cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-            
-            # FisherFace
-            if result[1] < 500:
-                cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-            else:
-                cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-            '''
+        
             # LBPHFace
             if result[1] < 70:
                 cv2.putText(frame_input, "godines", (x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
